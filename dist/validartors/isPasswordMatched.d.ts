d60c608271344511d701888d9285a985c926ee53
kobir1989 <kobir.h.ritu@gmail.com>
2023-09-05 13:05:55 +0600

    fix: tscconfig file declaration issue

---
+++ dist/validartors/isPasswordMatched.d.ts
@@ -0,0 +1,9 @@
+/***
+ * @function isPasswordMatched
+ * @param {password}
+ * @param {confirmPassword}
+ * @param {length}
+ * @return {string}
+ *
+ */
+export declare const isPasswordMatched: (password: string, confirmPassword: string, length: number) => boolean;
---
+++ dist/validartors/isValidEmail.d.ts
@@ -0,0 +1,6 @@
+/***
+ * @function isValidEmail takes an email address as input and uses the test() method of the regex pattern to check if the email matches the given pattern.
+ * @param {email}
+ * @return {boolean}
+ */
+export declare const isValidEmail: (email: string) => boolean;
