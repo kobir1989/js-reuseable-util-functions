647a4c69995480ebf08c011dac1449269db266f7
kobir1989 <kobir.h.ritu@gmail.com>
2023-09-07 11:48:37 +0600

    refactor: test files

--- test/validators/isValidEmail.test.ts
+++ test/validators/isValidEmail.test.ts
@@ -1,37 +1,39 @@
 import { isValidEmail } from '../../src/validartors/isValidEmail'
 
-test('Returns true if valid email with subdomain', () => {
-  expect(isValidEmail('test@sub.example.com')).toBe(true)
-})
+describe('isValidEmail validator function', () => {
+  it('Returns true if valid email with subdomain', () => {
+    expect(isValidEmail('it@sub.example.com')).toBe(true)
+  })
 
-test('Returns true if valid email with uppercase characters', () => {
-  expect(isValidEmail('Test.User@example.com')).toBe(true)
-})
+  it('Returns true if valid email with uppercase characters', () => {
+    expect(isValidEmail('it.User@example.com')).toBe(true)
+  })
 
-test('Returns true if valid email with numbers in local part', () => {
-  expect(isValidEmail('user123@example.com')).toBe(true)
-})
+  it('Returns true if valid email with numbers in local part', () => {
+    expect(isValidEmail('user123@example.com')).toBe(true)
+  })
 
-test('Returns false if email is empty', () => {
-  expect(isValidEmail('')).toBe(false)
-})
+  it('Returns false if email is empty', () => {
+    expect(isValidEmail('')).toBe(false)
+  })
 
-test('Returns false if email has leading spaces', () => {
-  expect(isValidEmail(' test@example.com')).toBe(false)
-})
+  it('Returns false if email has leading spaces', () => {
+    expect(isValidEmail(' it@example.com')).toBe(false)
+  })
 
-test('Returns false if email has trailing spaces', () => {
-  expect(isValidEmail('test@example.com ')).toBe(false)
-})
+  it('Returns false if email has trailing spaces', () => {
+    expect(isValidEmail('it@example.com ')).toBe(false)
+  })
 
-test('Returns false if email is missing "@" symbol', () => {
-  expect(isValidEmail('testexample.com')).toBe(false)
-})
+  it('Returns false if email is missing "@" symbol', () => {
+    expect(isValidEmail('itexample.com')).toBe(false)
+  })
 
-test('Returns false if email is missing top-level domain', () => {
-  expect(isValidEmail('test@example')).toBe(false)
-})
+  it('Returns false if email is missing top-level domain', () => {
+    expect(isValidEmail('it@example')).toBe(false)
+  })
 
-test('Returns false if email has invalid characters', () => {
-  expect(isValidEmail('test@ex@mple.com')).toBe(false)
+  it('Returns false if email has invalid characters', () => {
+    expect(isValidEmail('it@ex@mple.com')).toBe(false)
+  })
 })
--- test/validators/isValidFileExtention.test.ts
+++ test/validators/isValidFileExtention.test.ts
@@ -1,27 +1,29 @@
 import { isValidFileExtension } from '../../src/validartors/isValidFileExtention'
 
-test('should return true for a valid file extension', () => {
-  const allowedExtensions = ['jpg', 'png', 'gif']
-  const fileName = 'myimage.jpg'
-  expect(isValidFileExtension(allowedExtensions, fileName)).toBe(true)
-})
+describe('isValidFileExtension validator function', () => {
+  it('should return true for a valid file extension', () => {
+    const allowedExtensions = ['jpg', 'png', 'gif']
+    const fileName = 'myimage.jpg'
+    expect(isValidFileExtension(allowedExtensions, fileName)).toBe(true)
+  })
 
-test('should return false for an invalid file extension', () => {
-  const allowedExtensions = ['jpg', 'png', 'gif']
-  const fileName = 'document.pdf'
-  expect(isValidFileExtension(allowedExtensions, fileName)).toBe(false)
-})
+  it('should return false for an invalid file extension', () => {
+    const allowedExtensions = ['jpg', 'png', 'gif']
+    const fileName = 'document.pdf'
+    expect(isValidFileExtension(allowedExtensions, fileName)).toBe(false)
+  })
 
-test('should be case-insenstestive for file extensions', () => {
-  const allowedExtensions = ['jpg', 'png', 'gif']
-  const fileName = 'myimage.PNG' // Note the uppercase extension
-  expect(isValidFileExtension(allowedExtensions, fileName)).toBe(true)
-})
+  it('should be case-insensitive for file extensions', () => {
+    const allowedExtensions = ['jpg', 'png', 'gif']
+    const fileName = 'myimage.PNG' // Note the uppercase extension
+    expect(isValidFileExtension(allowedExtensions, fileName)).toBe(true)
+  })
 
-test('should throw an error for an empty fileName', () => {
-  const allowedExtensions = ['jpg', 'png', 'gif']
-  const fileName = ''
-  expect(() => isValidFileExtension(allowedExtensions, fileName)).toThrow(
-    `The fileName "${fileName}" does not have a valid file extension.`
-  )
+  it('should throw an error for an empty fileName', () => {
+    const allowedExtensions = ['jpg', 'png', 'gif']
+    const fileName = ''
+    expect(() => isValidFileExtension(allowedExtensions, fileName)).toThrow(
+      `The fileName "${fileName}" does not have a valid file extension.`
+    )
+  })
 })
